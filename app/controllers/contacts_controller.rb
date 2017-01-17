9f54577210e95f1e42d9b4529791accace8adaab
Ricky Lau <nreekay324@yahoo.com>
2017-01-17 02:26:13 +0000

    Improved error messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,9 +6,11 @@ class ContactsController < ApplicationController
    def create
       @contact = Contact.new(contact_params)
       if @contact.save
-         redirect_to new_contact_path, notice: "Message sent. Thanks!"
+         flash[:success] = "Message sent."
+         redirect_to new_contact_path
       else
-         redirect_to new_contact_path, notice: "Sorry, form is Incomplete."
+         flash[:error] = @contact.errors.full_messages.join(", ")
+         redirect_to new_contact_path
       end
    end
    
